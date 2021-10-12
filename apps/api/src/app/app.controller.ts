6883856e864937d549cfb43b5a5f5c47bee96f93
liang <fconliang@farmconnect.co.kr>
2021-10-12 21:11:35 +0900

    chore: remove auto generated library

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,16 +1,8 @@
 import { Controller, Get, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices'
-import { timeout } from 'rxjs/operators';
-import { Message } from '@lib/api-interfaces';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, @Inject('REDIS') private redis: ClientProxy) {}
-
-  @Get('hello')
-  getData(): Message {
-    return this.appService.getData();
-  }
-
+  // constructor(private readonly appService: AppService, @Inject('REDIS') private redis: ClientProxy) {}
 }
--- apps/edge-mon/src/app/app.component.ts
+++ apps/edge-mon/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@lib/api-interfaces';
 
 @Component({
   selector: 'smart-farm-prediction-root',
@@ -8,6 +7,7 @@ import { Message } from '@lib/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
+  // hello$ = this.http.get<Message>('/api/hello');
+
   constructor(private http: HttpClient) {}
 }
