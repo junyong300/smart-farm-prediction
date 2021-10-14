0099b0eb4c1661283b9d22f04f4ce1c980b52aef
liang <fconliang@farmconnect.co.kr>
2021-10-14 13:39:48 +0900

    feat(db): db configuration by common.conf

--- libs/db/src/db.module.ts
+++ libs/db/src/db.module.ts
@@ -1,5 +1,7 @@
 import { Module, Global } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonConfigModule } from '@lib/config';
+import { ConfigService } from '@nestjs/config';
 import { NamingStrategy } from './naming.strategy';
 import { DbService } from './db.service';
 import { Device } from './device';
@@ -8,17 +10,21 @@ import { Internal } from './env';
 @Global()
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '.fc12#$',
-      database: 'farmconnect',
-      entities: [__dirname + "/device/*.entity.ts", __dirname + "/env/*.entity.ts"],
-      autoLoadEntities: true,
-      namingStrategy: new NamingStrategy(),
-      logging: true
+    CommonConfigModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        type: config.get<"postgres"|"mysql">('DB_TYPE'),
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+        entities: [__dirname + "/device/*.entity.ts", __dirname + "/env/*.entity.ts"],
+        autoLoadEntities: true,
+        namingStrategy: new NamingStrategy(),
+        logging: true
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([Device, Internal])
   ],
