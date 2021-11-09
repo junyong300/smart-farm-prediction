7035c168e4710a22ee2db15e86d749608ca761aa
liang <fconliang@farmconnect.co.kr>
2021-11-09 15:35:08 +0900

    fix: fix config path on loading

--- libs/config/src/config.module.ts
+++ libs/config/src/config.module.ts
@@ -3,17 +3,17 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as path from 'path';
 
 const MODE = process.env.NODE_ENV;
-const DEV_CONFIG_FILE = process.argv.slice(2)[0];
+const PARAM_CONFIG_FILE = process.argv.slice(2)[0];
+const midDir = MODE?.startsWith('dev') ? '/devenv': '';
+const configFile = PARAM_CONFIG_FILE ? PARAM_CONFIG_FILE : path.join(process.cwd(), midDir, 'config', 'common.conf');
+console.log("configFile: " + configFile);
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
       // use args or use different conf file by NODE_ENV
-      envFilePath: [DEV_CONFIG_FILE ? DEV_CONFIG_FILE :
-        (MODE && MODE.startsWith('dev')) ? 'resources/config/common.conf' : path.join(__dirname, '..', 'config/common.conf'),
-        path.join(__dirname, '..', 'config/common.conf')
-      ]
+      envFilePath: [configFile]
     })
   ],
   controllers: [],
