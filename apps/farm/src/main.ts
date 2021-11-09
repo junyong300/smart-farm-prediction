560242de75e692ee60aa6b337ae3f004020405a4
liang <fconliang@farmconnect.co.kr>
2021-11-09 15:34:34 +0900

    feat(farm): Farm app skeleton

---
+++ apps/farm/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
+import { LogWrapper } from '@libs/config';
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const logWrapper = new LogWrapper("farm");
+  const app = await NestFactory.create(AppModule, {logger: logWrapper});
+  const configService = app.get<ConfigService>(ConfigService);
+  const logLevel = configService.get<string>('FARM_LOG_LEVEL', 'info');
+  logWrapper.setLogLevel(logLevel);
+
+  app.connectMicroservice({
+    transport: Transport.REDIS,
+    options: {
+        url: 'redis://' + configService.get<string>('REDIS_HOST', 'localhost') + ':' + configService.get<number>('REDIS_PORT', 6379)
+    }
+  });
+
+  await app.startAllMicroservices();
+}
+
+bootstrap();
